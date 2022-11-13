70e495466616675cb4e31fb2a82136ca6d178e25
Egor Vorontsov <sdoregor@sdore.me>
2022-11-13 15:29:49 +0300

    Fixed `__main__.py` and some typos.

--- __main__.py
+++ __main__.py
@@ -1,3 +1,34 @@
-if (__name__ == '__main__'):
-	from . import exit, main
-	raise exit(main())
+#!/usr/bin/env python3
+
+from utils import *
+from . import *
+
+def main():
+	utils_args = {i.dest for i in argparser._actions} # a hack, yep. TODO: do not hack
+	argparser.add_argument('img', metavar='<path | url>')
+	argparser.add_argument('-size', metavar='<maxSide | WxH>', default=-1)
+	argparser.add_argument('-padding', metavar='N', type=int)
+	argparser.add_argument('-padchar', metavar='char')
+	argparser.add_argument('-bgcolor', metavar='color')
+	argparser.add_argument('-resample', metavar='mode')
+	argparser.add_argument('--ascii', action='store_true')
+	argparser.add_argument('--noansi', action='store_true')
+	cargs = argparser.parse_args()
+
+	kwargs = dict(filter(operator.itemgetter(1), S(cargs.__dict__).translate({'double_vres': 'ascii'}, strict=False)(*({i.dest for i in argparser._actions}-utils_args)).items()))
+	size = Sstr(kwargs['size']).bool(minus_one=False)
+	kwargs['size'] = tuple(map(int, kwargs['size'].split('x'))) if (size) else os.get_terminal_size()
+	if ('resample' in kwargs): kwargs['resample'] = getattr(Image, kwargs['resample'].upper())
+	if (kwargs.pop('noansi', False)): kwargs['chars'] = ' ░▒▓█'
+	if (not size): sys.stderr.write('\033c')
+	sys.stdout.write(showimg(**kwargs)); sys.stdout.flush()
+	if (size): sys.stderr.write('\n'); return
+	try: sys.stdin.read(1)
+	except: pass
+	sys.stderr.write('\033c')
+	sys.stderr.flush()
+
+if (__name__ == '__main__'): exit(main(), nolog=True)
+
+# by Sdore, 2022
+#  www.sdore.me
--- cimg.py
+++ cimg.py
@@ -12,10 +12,10 @@ def showimg(img, size, chars='█', *, padding=0, padchar=' ', bgcolor=None, dou
 	Parameters:
 		img: PIL Image, Image.open arg or URL as str.
 		size: (w, h) int tuple or single int to use as length of one or both sides of output.
-		chars='█': multiple chars (i.e. '░▒▓█') to use as b/w palette or single (i.e. '█') for RGB (ANSI-escape) mode.
+		chars='█': multiple chars (e.g. '░▒▓█') to use as b/w palette or single (e.g. '█') for RGB (ANSI-escape) mode.
 		padding=0: prefix every line with N `padchar`s.
 		padchar=' ': character to use as left-padding.
-		double_vres=False: double the vertical size and use bg color as the bottom pixel for half-block chars (i.e. '▀').
+		double_vres=False: double the vertical size and use bg color as the bottom pixel for half-block chars (e.g. '▀').
 		resample=0: Image.resize() resampling mode.
 	"""
 	img = openimg(img)
@@ -63,31 +63,5 @@ def openimg(img):
 		except Exception: ex = True
 		if (ex): raise
 
-def main():
-	utils_args = {i.dest for i in argparser._actions} # a hack, yep. TODO: do not hack
-	argparser.add_argument('img', metavar='<path | url>')
-	argparser.add_argument('-size', metavar='<maxSide | WxH>', default=-1)
-	argparser.add_argument('-padding', metavar='N', type=int)
-	argparser.add_argument('-padchar', metavar='char')
-	argparser.add_argument('-bgcolor', metavar='color')
-	argparser.add_argument('-resample', metavar='mode')
-	argparser.add_argument('--ascii', action='store_true')
-	argparser.add_argument('--noansi', action='store_true')
-	cargs = argparser.parse_args()
-
-	kwargs = dict(filter(operator.itemgetter(1), S(cargs.__dict__).translate({'double_vres': 'ascii'}, strict=False)(*({i.dest for i in argparser._actions}-utils_args)).items()))
-	size = Sstr(kwargs['size']).bool(minus_one=False)
-	kwargs['size'] = tuple(map(int, kwargs['size'].split('x'))) if (size) else os.get_terminal_size()
-	if ('resample' in kwargs): kwargs['resample'] = getattr(Image, kwargs['resample'].upper())
-	if (kwargs.pop('noansi', False)): kwargs['chars'] = ' ░▒▓█'
-	if (not size): sys.stderr.write('\033c')
-	sys.stdout.write(showimg(**kwargs)); sys.stdout.flush()
-	if (size): sys.stderr.write('\n'); return
-	try: sys.stdin.read(1)
-	except: pass
-	sys.stderr.write('\033c')
-	sys.stderr.flush()
-
-if (__name__ == '__main__'): exit(main(), nolog=True)
-
-# by Sdore, 2020
+# by Sdore, 2020-22
+#   www.sdore.me
