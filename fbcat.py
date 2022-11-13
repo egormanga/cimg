69ee0f88b3a58e4e5b6cd3ccc6cfb74b749ef094
Egor Vorontsov <sdoregor@sdore.me>
2022-11-13 15:38:37 +0300

    Added wrapper scripts; fixed some issues and code style.

--- fbcat.py
+++ fbcat.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
 # Write image to framebuffer
 
-import os, sys, requests
-from cimg import *
-from utils import *; logstart('fbcat')
+from utils import *
+from . import *
 
-def write(fb, img, size):
-	fb = open(fb, 'wb', buffering=-1)#2**31-1)
+@export
+def fbwrite(fb, img, size):
 	img = openimg(img)
 	img.thumbnail(size)
+
 	scr = Image.new('RGB', size)
 	scr.paste(img, (abs(img.size[0]-size[0])//2, 0))
 	data = scr.tobytes()
-	for i in range(len(data)//3): fb.write(bytes(data[i:i+4:-1])+b'\0')
 
-def main():
-	argparser.add_argument('fb', metavar='<fb>')
-	argparser.add_argument('res', metavar='<res>')
-	argparser.add_argument('img', metavar='<img>')
-	cargs = argparser.parse_args()
-	write(cargs.fb, cargs.img, tuple(map(int, cargs.res.split('x'))))
+	with open(fb, 'wb', buffering=-1) as fb: #2**31-1)
+		for i in range(len(data)//3):
+			fb.write(bytes(data[i:i+4:-1]) + b'\0')
 
-if (__name__ == '__main__'): logstarted(); main()
-else: logimported()
+@apmain
+@aparg('fb', metavar='<fb>')
+@aparg('res', metavar='<res>')
+@aparg('img', metavar='<img>')
+def main(cargs):
+	fbwrite(cargs.fb, cargs.img, tuple(map(int, cargs.res.split('x'))))
+
+if (__name__ == '__main__'): exit(main())
+
+# by Sdore, 2018-22
+#   www.sdore.me
--- fbtest.sh
+++ fbtest.sh
@@ -1,3 +1,5 @@
-#SIZE=1920x1080
-SIZE=1376x768
-convert -resize $SIZE -background black -gravity center -extent $SIZE $1 bgra:/dev/fb0
+#!/bin/sh
+
+SIZE=1920x1080
+
+exec convert -resize "$SIZE" -background black -gravity center -extent "$SIZE" "$1" bgra:'/dev/fb0'
